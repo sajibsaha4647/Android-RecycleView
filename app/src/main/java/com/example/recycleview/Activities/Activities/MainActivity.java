ab01b86d7edf1e8dc4bd5c5cada7878c3d56a293
sajibsaha4647 <sajibsahabdtask@gmail.com>
2022-01-31 13:37:04 +0600

    updated

--- app/src/main/java/com/example/recycleview/Activities/Activities/MainActivity.java
+++ app/src/main/java/com/example/recycleview/Activities/Activities/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.recycleview.Activities.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.example.recycleview.Activities.Myadaptar.MyListAdaptar;
 import com.example.recycleview.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,14 +19,29 @@ public class MainActivity extends AppCompatActivity {
     };
 
     String [] title,discript;
+    MyListAdaptar myListAdaptar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerView = findViewById(R.id.RecycleViewId);
-
         title = getResources().getStringArray(R.array.country_name);
         discript = getResources().getStringArray(R.array.country_details);
+        myListAdaptar = new MyListAdaptar(this,title,discript,images);
 
+        recyclerView.setAdapter(myListAdaptar);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        myListAdaptar.setOnItemClickListener(new MyListAdaptar.Clicklistener() {
+            @Override
+            public void onItemClick(int Position, View view) {
+                Toast.makeText(MainActivity.this,title[Position],Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onItemLongClick(int Position, View view) {
+                Toast.makeText(MainActivity.this,title[Position],Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/recycleview/Activities/Myadaptar/MyListAdaptar.java
+++ app/src/main/java/com/example/recycleview/Activities/Myadaptar/MyListAdaptar.java
@@ -1,17 +1,23 @@
 package com.example.recycleview.Activities.Myadaptar;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.recycleview.R;
 
 public class MyListAdaptar extends RecyclerView.Adapter<MyListAdaptar.Myviewholder> {
 
     Context context ;
     String [] title,desc;
     int [] Images;
+    private static Clicklistener clicklistener;
 
     public MyListAdaptar(Context context, String[] title, String[] desc, int[] images) {
         this.context = context;
@@ -22,23 +28,66 @@ public class MyListAdaptar extends RecyclerView.Adapter<MyListAdaptar.Myviewhold
 
     @NonNull
     @Override
-    public Myviewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public Myviewholder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+
+        LayoutInflater layoutInflater  = LayoutInflater.from(context);
+        View view = layoutInflater.inflate(R.layout.sample_layout,viewGroup,false);
+
+        return new Myviewholder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull Myviewholder holder, int position) {
 
+        holder.titleTextView.setText(title[position]);
+        holder.descTextview.setText(desc[position]);
+        holder.imageflag.setImageResource(Images[position]);
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+
+        return title.length;
     }
 
-    public class Myviewholder extends RecyclerView.ViewHolder {
+    public class Myviewholder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
+
+        TextView titleTextView,descTextview;
+        ImageView imageflag;
         public Myviewholder(@NonNull View itemView) {
             super(itemView);
+
+            titleTextView = itemView.findViewById(R.id.titleID);
+            descTextview = itemView.findViewById(R.id.descId);
+            imageflag = itemView.findViewById(R.id.imageId);
+
+            imageflag.setOnClickListener(this);
+            imageflag.setOnLongClickListener(this);
+
         }
+
+        @Override
+        public void onClick(View view) {
+            clicklistener.onItemClick(getAdapterPosition(),view);
+        }
+
+        @Override
+        public boolean onLongClick(View view) {
+            clicklistener.onItemLongClick(getAdapterPosition(),view);
+            return false;
+        }
+
+
+    }
+
+    public interface Clicklistener{
+        void onItemClick(int Position ,View view);
+        void onItemLongClick(int Position ,View view);
+    }
+
+    public void setOnItemClickListener(Clicklistener click){
+
+        MyListAdaptar.clicklistener = click ;
     }
 }
